a8bad5a5d819dc1ca8c142c9f6042235fa40018e
2051Code <2051Code@gmail.com>
2022-03-05 10:31:51 -0800

    ob9u;7g878

--- src/main/java/frc/robot/Subsystems/ClimbControls.java
+++ src/main/java/frc/robot/Subsystems/ClimbControls.java
@@ -4,38 +4,19 @@
 
 package frc.robot.Subsystems;
 
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.OI;
-import frc.robot.RobotMap;
 
 public class ClimbControls extends SubsystemBase {
-  public OI m_oi = new OI();
 
-  private CANSparkMax ClimbMotor1 = new CANSparkMax(RobotMap.ClimbMotor1, MotorType.kBrushed);
-  private CANSparkMax ClimbMotor2 = new CANSparkMax(RobotMap.ClimbMotor2, MotorType.kBrushed);
+public OI m_oi = new OI();
 
 
   /** Creates a new ClimbControls. */
-
   public ClimbControls() {}
-  
-  public void SetClimbSpeed(){
-    {
-    if(m_oi.getRightBumper())
-    {
-      ClimbMotor1.set(.5);
-      ClimbMotor2.set(.5);
-    }else{
-      ClimbMotor1.set(0);
-      ClimbMotor2.set(0);
-      }
-    }
 
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
